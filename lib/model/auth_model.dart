592f60837371b4eb6b4342b96241b627012e3479
Khang Huynh Vy <khang.huynh@bstarsolutions.com>
2024-05-20 14:18:38 +0700

    fix dart conventions

--- lib/model/auth_model.dart
+++ lib/model/auth_model.dart
@@ -3,5 +3,9 @@ class AuthModel {
   final String refreshToken;
   final String expiredTime;
 
-  AuthModel({required this.accessToken, required this.refreshToken, required this.expiredTime});
+  AuthModel({
+    required this.accessToken,
+    required this.refreshToken,
+    required this.expiredTime,
+  });
 }
